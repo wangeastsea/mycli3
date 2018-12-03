598b41469809f2d4a8210d635503cebac38ce187
王东海 <wangdonghai@bogon.ucredit.com>
2018-12-03 17:03:32 +0800

    构建多页应用

---
+++ build/utils.js
@@ -0,0 +1,107 @@
+/* utils.js */
+const path = require('path');
+
+// glob 是 webpack 安装时依赖的一个第三方模块，还模块允许你使用 * 等符号,
+// 例如 lib/*.js 就是获取 lib 文件夹下的所有 js 后缀名的文件
+const glob = require('glob');
+
+// 取得相应的页面路径，因为之前的配置，所以是 src 文件夹下的 pages 文件夹
+const PAGE_PATH = path.resolve(__dirname, '../src/pages');
+
+/* 
+ * 多入口配置
+ * 通过 glob 模块读取 pages 文件夹下的所有对应文件夹下的 js * 后缀文件，如果该文件存在
+ * 那么就作为入口处理
+ */
+exports.getEntries = () => {
+    let entryFiles = glob.sync(PAGE_PATH + '/*/*.js') // 同步读取所有入口文件
+    let map = {}
+
+    // 遍历所有入口文件
+    entryFiles.forEach(filePath => {
+        // 获取文件名
+        let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+
+        // 以键值对的形式存储
+        map[filename] = filePath
+    })
+
+    return map
+}
+
+exports.htmlPlugin = configs => {
+    let entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+    let arr = []
+
+    entryHtml.forEach(filePath => {
+        let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+        let conf = {
+            template: filePath, // 模板路径
+            filename: filename + '.html', // 生成 html 的文件名
+            chunks: ['manifest', 'vendor', filename],
+            inject: true,
+        }
+
+        // 如果有自定义配置可以进行 merge
+        if (configs) {
+            conf = merge(conf, configs)
+        }
+
+        // 针对生产环境配置
+        if (process.env.NODE_ENV === 'production') {
+            conf = merge(conf, {
+                minify: {
+                    removeComments: true, // 删除 html 中的注释代码
+                    collapseWhitespace: true, // 删除 html 中的空白符
+                    // removeAttributeQuotes: true // 删除 html 元素中属性的引号
+                },
+                chunksSortMode: 'manual' // 按 manual 的顺序引入
+            })
+        }
+        arr.push(new HtmlWebpackPlugin(conf))
+    })
+
+    return arr
+}
+
+// pages 多入口配置
+exports.setPages = configs => {
+    let entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+    let map = {}
+
+    entryFiles.forEach(filePath => {
+        let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+        let tmp = filePath.substring(0, filePath.lastIndexOf('\/'))
+
+        let conf = {
+            // page 的入口
+            entry: filePath,
+            // 模板来源
+            template: tmp + '.html',
+            // 在 dist/index.html 的输出
+            filename: filename + '.html',
+            // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
+            chunks: ['manifest', 'vendor', filename],
+            inject: true,
+        };
+
+        if (configs) {
+            conf = merge(conf, configs)
+        }
+
+        if (process.env.NODE_ENV === 'production') {
+            conf = merge(conf, {
+                minify: {
+                    removeComments: true, // 删除 html 中的注释代码
+                    collapseWhitespace: true, // 删除 html 中的空白符
+                    // removeAttributeQuotes: true // 删除 html 元素中属性的引号
+                },
+                chunksSortMode: 'manual' // 按 manual 的顺序引入
+            })
+        }
+
+        map[filename] = conf
+    })
+
+    return map
+}
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const configs = require('./config')
 const merge = require('webpack-merge')
 const cfg = process.env.NODE_ENV === 'production' ? configs.build.env : configs.dev.env
+const utils = require('./build/utils')
 module.exports = {
     // 二级目录
     baseUrl: '/wangdonghai',
@@ -20,6 +21,16 @@ module.exports = {
                 return args
             })
     },
+    // 多页应用配置
+    // configureWebpack: config => {
+    //     config.entry = utils.getEntries()
+    //     // 使用 return 一个对象会通过 webpack-merge 进行合并，plugins 不会置空
+    //     return {
+    //         plugins: [...utils.htmlPlugin()]
+    //     }
+    // },
+    // pages多页设置
+    pages: utils.setPages(),
     // 开发模式下启动本地服务
     devServer: {
         open: true,
